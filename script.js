2c852423bae5a19d1aaeb062505a9fe716b9083c
hencci <hencci20@gmail.com>
2024-08-15 17:24:42 +0100

    Updated file

--- script.js
+++ script.js
@@ -5,5 +5,24 @@ function fetchWeather(location) {
     loadingDiv.classList.remove('hidden');
 
     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=DBELFYET2HB66AN6SZC56BC6M`, { mode: 'cors' })
-        
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        const conditions = data.currentConditions.conditions;
+        const fahrenheitTemp = data.currentConditions.temp;
+        const celsiusTemp = fahrenheitToCelsius(fahrenheitTemp);
+        const feelsLikeCelsius = fahrenheitToCelsius(data.currentConditions.feelslike);
+        const humidity = data.currentConditions.humidity;
+        const windSpeed = data.currentConditions.windspeed;
+        const chanceOfRain = data.days[0].precipprob || 0;
+
+        document.getElementById('conditions').textContent = conditions;
+        document.getElementById('locationDisplay').textContent = location;
+        document.getElementById('temperature').textContent = `${celsiusTemp} °C`;
+        document.getElementById('feelsLike').textContent = `${feelsLikeCelsius} °C`;
+        document.getElementById('humidity').textContent = `${humidity} %`;
+        document.getElementById('chanceOfRain').textContent = `${chanceOfRain} %`;
+        document.getElementById('windSpeed').textContent = `${windSpeed} km/h`;
+    })
 }
