dfd3e054f137b1d6e91fe9d2a6437a206cac99b6
hencci <hencci20@gmail.com>
2024-08-15 16:47:03 +0100

    Removed hidden class

---
+++ script.js
@@ -0,0 +1,6 @@
+function fetchWeather(location) {
+    const weatherInfoDiv = document.getElementById('weatherContainer');
+    const loadingDiv = document.getElementById('loading');
+
+    loadingDiv.classList.remove('hidden');
+}
